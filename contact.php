650dfba3687900a614685b06e132285ae5e05346
bobderrico80 <bobderrico80@gmail.com>
2014-04-17 22:46:03 -0400

    Content updates
    Added content to index and about pages.  Updated CSS for headings and
    paragraphs, began to work on slideshow for current projects page.

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Derrico Web Design - Custom Websites for Small Businesses, Organizations, and Individuals</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<img src="img/banner.png"/>
+			</div>
+			<div id="navbarContainer">
+				<div id="navbar">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li class="spacer"></li>
+						<li><a href="about.html">About</a></li>
+						<li class="spacer"></li>
+						<li><a href="projects.html">Current Projects</a></li>
+						<li class="spacer"></li>
+						<li><a href="contact.php" class="current">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id="content">
+			</div>
+			<div id="footer">
+				<p>Copyright &copy 2014 Derrico Web Design</p>
+			</div>
+		</div>
+	</body>
+</html>
