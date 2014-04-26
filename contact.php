11c52216bff154db140727d5ef37931ebf2b9222
bobderrico80 <bobderrico80@gmail.com>
2014-04-25 22:09:14 -0400

    Site build completed
    FInished contact form.  Added a favicon.  Basic design of the site at
    this point is complete.

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@
 	<head>
 		<title>D'Errico Web Design - Custom Websites for Small Businesses, Organizations, and Individuals</title>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+		<link rel="icon" type="image/png" href="img/favicon.png"/>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		
 		
 	</head>
 	<body>
@@ -26,35 +26,34 @@
 				</div>
 			</div>
 			<div id="content">
-				
-							
-				<h1>Contact D'Errico Web Design</h1>
-				<p class="required">* - required fields</p>
-				
+												
 				<?php
 					//display form if user has not clicked submit
 					if (!isset($_POST["submit"])) {
 						?>
-						
-						
+						<h1 class="noBottomMargin">Contact D'Errico Web Design</h1>
+						<p class="required">* - required fields</p>
 						<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-						From: <input type="text" name="from" id="from"><span class="required" id="fromErr">*</span><br>
-						Subject: <input type="text" name="subject" id="subject"><span class="required" id="subjectErr">*</span><br>
-						Message: <textarea rows="10" cols="40" name="message" id="message"></textarea><span class="required" id="messageErr">*</span><br>
-						<input type="submit" name="submit" id="submit" value="Submit Feedback" disabled>
+						<span class="spaced">E-Mail Address:</span><input type="text" name="from" id="from"><span class="required" id="fromErr">*</span><br>
+						<span class="spaced">Name:</span><input type="text" name="name" id="name"><span class="required" id="nameErr">*</span><br><br>
+						Message: <span class="required" id="messageErr">*</span><br>
+						<textarea rows="20" cols="80" name="message" id="message"></textarea><br>
+						<input type="submit" name="submit" id="submit" value="Send Message" disabled>
 						</form>
 						<?php
 					} else { //user has submitted the form
-						//check if the "from" input field is filled out
-						if (isset($_POST["from"])) {
-							$from = spamcheck($_POST["from"]);
-							$subject = $_POST["subject"];
-							$message = $_POST["message"];
-							$message = wordwrap($message, 70);
-							//send mail
-							mail("bob@derricowebdesign.com", $subject, $message, "From: $from\n");
-							echo "Thank you for sending us feedback";
-							}
+						$from = spamcheck($_POST["from"]);
+						$subject = "[DerricoWebDesign] New Message from " . $_POST["name"];
+						$message = $_POST["message"];
+						$message = wordwrap($message, 70);
+						//send mail
+						mail("bob@derricowebdesign.com", $subject, $message, "From: $from\n");
+						?>
+						<h1 class="noBottomMargin">Thank You!</h1>
+						<p>Your message has been sent.  You should expect to hear from us within 24 hours.
+						
+						<p><a href="contact.php">Send Another Message</a> | <a href="index.html">Return Home</a></p>
+						<?php
 						}
 				?>
 				
@@ -74,7 +73,7 @@
 				//functions for validating form fields
 				//variables for determining which fields have been properly completed
 				var fromFlag = false;
-				var subjectFlag = false;
+				var nameFlag = false;
 				var messageFlag = false;
 				
 				//on from field blur
@@ -89,7 +88,7 @@
 					}
 					
 					//checks if from field is properly formatted
-					var regexEmail = new RegExp("\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b","i");
+					var regexEmail = /\b[a-z0-9.-_%+]+@[a-z0-9.-]+\.[a-z]{2,4}\b/i;
 					var from = $("#from").val();
 					if (!regexEmail.test(from)) {
 						$("#fromErr").text("* Email address is not valid");
@@ -99,45 +98,45 @@
 						fromFlag = true;
 					}
 					
-					//checks status of flags and enables submit button if all are true.
-					if (fromFlag && subjectFlag && messageFlag) {
-						$("#submit").attr("disabled","false");
-					}
+					checkSubmit();
 				});
 				
-				//on subject field blur
-				$("#subject").blur (function() {
+				//on name field blur
+				$("#name").blur (function() {
 					//checks if the subject field is completed
-					if ($("#subject").val() == "") {
-						$("#subjectErr").text("* Subject field must be completed");
+					if ($("#name").val() == "") {
+						$("#nameErr").text("* Please enter your name");
 						subjectFlag = false;
 					} else {
-						$("#subjectErr").text("*");
-						subjectFlag = true;
+						$("#nameErr").text("*");
+						nameFlag = true;
 					}
 					
-					//checks status of flags and enables submit button if all are true.
-					if (fromFlag && subjectFlag && messageFlag) {
-						$("#submit").attr("disabled","false");
-					}
+					checkSubmit();
 				});
 				
-				//on message field blur
-				$("#message").blur (function() {
+				//on message field keyup
+				$("#message").keyup (function() {
 					//checks if the message field is completed
 					if ($("#message").val() == "") {
-						$("#messageErr").text("* Message field must be completed");
+						$("#messageErr").text("* Please enter a message");
 						messageFlag = false;
 					} else {
 						$("#messageErr").text("*");
 						messageFlag = true;
 					}
 					
-					//checks status of flags and enables submit button if all are true.
-					if (fromFlag && subjectFlag && messageFlag) {
-						$("#submit").attr("disabled","false");
-					}
+					checkSubmit();
 				});
+				
+				//function to check the status of flags and enable submit button if all are true.
+				function checkSubmit() {
+					if (fromFlag && nameFlag && messageFlag) {
+						$("#submit").removeAttr("disabled");
+					} else {
+						$("#submit").attr("disabled","true");
+					}
+				}
 			</script>
 			
 		</div>
