393fd1a5d203bfbf8b17fb612564aae68f2afccf
bobderrico80 <bobderrico80@gmail.com>
2014-07-14 12:00:55 -0400

    Added favicon, Improved about section
    About section now pulls content from a custom-made cms.

--- index.php
+++ index.php
@@ -110,39 +110,13 @@ try {
                         Web Designer/Developer
                     </p>
                 </div>
-                <p>
-                    Bob D'Errico is a web designer/developer from suburban Philadelphia.  
-                    His expertise lies in creating concise and impressive websites for 
-                    businesses, non-profits, organizations, and school groups.  
-                    His experience in the education field as a band and choral director 
-                    gives him special insight into the needs of these clients.  
-                </p>
-                <p>
-                    Bob can also create web apps for specific needs; his 
-                    <a href="http://www.rhythmrandomizer.com" target="_blank">Rhythm Randomizer</a> 
-                    web app gives music educators a quick and easy way to generate 
-                    an infinite number of rhythm drills for their students. This 
-                    site is currently free! He recently developed a <a href="http://whsmb.derricowebdesign.com" target="_blank">web app</a> 
-                    that allows band students to access their music from home, including 
-                    midi play-along files and specific instrumentation set-up. Bob 
-                    can create other web sites and apps, tailored to your organization's 
-                    specific needs. 
-                </p>
-                <p>
-                    <strong>At this time, in addition to regular business website design, 
-                    Bob is taking a limited number of non-profit or school clients 
-                    to help them grow - free of charge! <a href="contact.php" class="contactLink">Contact him</a>
-                    for details!</strong>
-                </p>
-                <p>
-                    When he is not working with web design, Bob enjoys working with 
-                    music-recording technology, cooking, and spending time with 
-                    his wife, and two daughters. 
-                </p>
-                <p>
-                    Contact <a href="contact.php" class="contactLink">Bob D'Errico</a> 
-                    here to start improving your group's presence and accessibility online. 
-                </p>
+                <?php
+                    //Get About content from cms table
+                    $rstAbout = $db->query('SELECT cmsContent from cms WHERE cmsID = 2');
+                    while ($rowAbout = $rstAbout->fetch()) {
+                        echo '<p>' . preg_replace('/\r\n/', '</p><p>', $rowAbout[0]) . '</p>';
+                    }
+                ?>
             </div>
             
             <!--Contact Section-->
